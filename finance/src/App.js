ca2ccdfaf4e66dc1fa40832ae75ef70c8cc60ac7
zhitingli12 <zhitingli54678@gmail.com>
2024-04-06 12:34:45 -0400

    pu

--- finance/src/App.js
+++ finance/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+      
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ finance/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div> className="navbar"
+        <div className="left"></div>
+        <div className="right">
+            <Link to="/"> Home</Link>
+            <Link to="/about"> About</Link>
+            <Link to="/survey"> Servey</Link>
+            <Link to="/resoures"> Resources</Link>
+    </div>
+    </div>
+  );
+}
+
+export default Navbar
