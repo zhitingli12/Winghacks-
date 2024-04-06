aaa3f99dbe561e5599910af795a48d4b93db277c
zhitingli12 <zhitingli54678@gmail.com>
2024-04-06 16:27:25 -0400

    pu

--- finance/src/pages/Home.js
+++ finance/src/pages/Home.js
@@ -5,7 +5,7 @@ import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className="home" style={{backgroundImage: 'url(${homeImage})'}}>
+    <div className="home" style={{backgroundImage:`url(${homeImage})`}}>
       <div className="headerContainer">
         <h1>Name of Company</h1>
       </div>
