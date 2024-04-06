88cb30df9fc80881b1acad9fe79f4a63fbdc9432
Jaden Lindner <jadenlindner@ufl.edu>
2024-04-06 15:46:40 -0400

    idk

--- finance/src/components/Navbar.js
+++ finance/src/components/Navbar.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from "react-router-dom";
 import "../styles/Navbar.css";
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
   return (
     <div className="navbar">
-        <div className="left"></div>
-        <div className="right">
+        
             <Link to="/"> Home</Link>
             <Link to="/about"> About</Link>
-            <Link to="/survey"> Servey</Link>
-            <Link to="/resoures"> Resources</Link>
-        </div>
+            <Link to="/survey"> Survey</Link>
+            <Link to="/resources"> Resources</Link>
+        
+
     </div>
   );
 }
--- finance/src/pages/Home.js
+++ finance/src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 import homeImage from "../picture/homeImage.jpg";
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className="home">
-      <div className="header"style={{backgroundImage: 'url(${homeImage})'}}>
+    <div className="home" style={{backgroundImage: 'url(${homeImage})'}}>
+      <div className="headerContainer">
         <h1>Name of Company</h1>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home
