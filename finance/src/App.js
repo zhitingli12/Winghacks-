4a709a68f63fa41bd6e48bc1009df60b81974fa5
zhitingli12 <zhitingli54678@gmail.com>
2024-04-06 15:22:56 -0400

    pu

--- finance/src/App.js
+++ finance/src/App.js
@@ -1,4 +1,25 @@
-import { App } from './App.1';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Resources from "./pages/Resources";
+import About from "./pages/About";
+import Survey from "./pages/Survey";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/resources" exact component={Resources} />
+          <Route path="/about" exact component={About} />
+          <Route path="/survey" exact component={Survey} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
--- finance/src/pages/Home.js
+++ finance/src/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import {Link} from "ract-rounter-dom";
-import homeImage from "../picture/homeImage.jeg";
+import {Link} from "react-router-dom";
+import homeImage from "../picture/homeImage.jpg";
 
 function Home() {
   return (
     <div className="home">
-      <div className="header" 
-      style={{backgroundImage: 'url(${homeImage})'}>
+      <div className="header"style={{backgroundImage: 'url(${homeImage})'}}>
         <h1>Name of Company</h1>
       </div>
     </div>
