df2e61a1442082012aa9fd75bd7e27c8b1dad2b6
Jaden Lindner <jadenlindner@ufl.edu>
2024-04-06 23:58:09 -0400

    Update Home.js

--- finance/src/pages/Home.js
+++ finance/src/pages/Home.js
@@ -7,7 +7,7 @@ function Home() {
     <div className="home" style={{backgroundImage:`url(${homeImage})`}}>
       <div className="headerContainer" style={{opacity: 0.81, backgroundColor: `rgb(75,143,186)`}}>
         <h1 style={{fontSize: "60px"}}>Financial Arsenal</h1>
-        <h2 style={{margin: "80px"}}>We serves to positively contribute to users' understanding of their personal finances and financial goals. 
+        <h2 style={{margin: "80px"}}>We strive to positively contribute to users' understanding of their personal finances and financial goals. 
           In other words, our goal is to assist you in building your personal financial arsenal, much like you would a 
           toolkit or skillset. We accomplish this through issuing a survey intended to assess an individual's financial 
           goals, suggesting actionable steps for addressing their personal needs based upon their survey responses, and 
