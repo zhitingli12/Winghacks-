0b25afde240008d57dad625c149025f50e763f20
zhitingli12 <zhitingli54678@gmail.com>
2024-04-06 21:42:06 -0400

    pu

--- finance/src/pages/Home.js
+++ finance/src/pages/Home.js
@@ -5,10 +5,16 @@ import "../styles/Home.css";
 function Home() {
   return (
     <div className="home" style={{backgroundImage:`url(${homeImage})`}}>
-      <div className="headerContainer">
-        <h1>Name of Company</h1>
+      <div className="headerContainer" style={{opacity: 0.81, backgroundColor: `rgb(75,143,186)`}}>
+        <h1 style={{fontSize: "60px"}}>Financial Arsenal</h1>
+        <h2 style={{margin: "80px"}}>We serves to positively contribute to users' understanding of their personal finances and financial goals. 
+          In other words, our goal is to assist you in building your personal financial arsenal, much like you would a 
+          toolkit or skillset. We accomplish this through issuing a survey intended to assess an individual's financial 
+          goals, suggesting actionable steps for addressing their personal needs based upon their survey responses, and 
+          providing relevant resources and information.</h2>
       </div>
     </div>
+
   );
 }
 
